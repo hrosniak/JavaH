3b5a1e8cbd7dc0b8d1d07e44fd7f49446924b69e
HubertR <hubertcfc@gmail.com>
2020-08-04 20:13:45 +0200

    JavaH

---
+++ Main/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "Hubert";
+        String b = "Rośniak";
+        String c = "wiek 26";
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
