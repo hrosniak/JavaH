0169270ecc616278e931cedf2f0006edb5a12ccf
HubertR <hubertcfc@gmail.com>
2020-08-04 21:10:35 +0200

    razem

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         int d = 6;
         short e = 7;
         double f = 1.5;
+        String maslo = "rama";
+        String mleko = "UHT";
+        String napoj = "woda" ;
+        String razem = maslo+ " " + mleko+ " " + napoj;
+        System.out.println(razem);
 
         System.out.println(d);
         System.out.println(e);
