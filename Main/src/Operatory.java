46d5f70f33724b2cd18408e5f2db461ee92b5765
HubertR <hubertcfc@gmail.com>
2020-08-06 06:53:26 +0200

    warunkiIf

--- Main/src/Operatory.java
+++ Main/src/Operatory.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class Operatory {
     int a = 1;
     int b = 1;
     int c = 3;
-    public void operatory(){
+    int number;
+
+    public void operatory() {
         System.out.println(a == b);
         System.out.println(b >= c);
         System.out.println(a <= b);
         System.out.println(a != b);
         System.out.println(a == c);
-        System.out.println((a == b )&&(a == c));
+        System.out.println((a == b) && (a == c));
         System.out.println(a == b || a < c);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jaką masz temperaturę: ");
+        int number = scanner.nextInt();
+        if (36 >= number) {
+            System.out.println("Mam stan podgorączkowy");
+        } else{
+            System.out.println("Jestem chory");
+        }
+
     }
 }
